3cf2d2015476b6d25d1741abb881a87c2a573cd7
yosuke1974 <yosuke.hayashi.11@gmail.com>
2019-04-28 00:01:06 +0900

    File replace

--- cifar10_dcgan_rev1.py
+++ cifar10_dcgan_rev1.py
@@ -159,12 +159,12 @@ def train(BATCH_SIZE):
             X = np.concatenate((image_batch, generated_images))
             y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
             d_loss = d_model.train_on_batch(X, y)
-            print("batch %d d_loss : %f" % (index, d_loss))
+            print("epoch %d batch %d d_loss : %f" % (epoch, index, d_loss))
             noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
             d_model.trainable = False
             g_loss = d_on_g.train_on_batch(noise, [1] * BATCH_SIZE)
             d_model.trainable = True
-            print("batch %d g_loss : %f" % (index, g_loss))
+            print("epoch %d batch %d g_loss : %f" % (epoch, index, g_loss))
             if index % 10 == 9:
                 g_model.save_weights('generator', True)
                 d_model.save_weights('discremenator', True)
