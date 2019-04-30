70cb4d01072d44e7fd845fa26017f4e8b435095a
yosuke1974 <yosuke.hayashi.11@gmail.com>
2019-04-30 16:36:15 +0900

    File replace

--- cifar10_dcgan_rev1.py
+++ cifar10_dcgan_rev1.py
@@ -1,8 +1,9 @@
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, UpSampling2D, MaxPooling2D, Reshape
+from keras.models import Sequential, Model
+from keras.layers import Input, Flatten, Dense, Conv2D, Reshape, LeakyReLU, Conv2DTranspose
 from keras.layers.core import Activation
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
+from keras.initializers import RandomNormal as RN, Constant
 
 #from keras.applications.vgg16 import VGG16
 from keras.datasets import cifar10
@@ -23,47 +24,95 @@ def main():
     elif args.mode == "generate":
         generate(BATCH_SIZE=args.batch_size, nice=args.nice)
 
-#Discreminatorモデル（VGG16をFineTuning）
+#Discreminatorモデル
 def D_model(Height, Width, channel=3):
-    model = Sequential()
-    model.add(
-            Conv2D(64, (5, 5),
-            padding='same',
-            input_shape=(Height, Width, 3))
-            )
-    model.add(Activation('tanh'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, (5, 5)))
-    model.add(Activation('tanh'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation('tanh'))
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
+    inputs = Input((Height, Width, channel))
+    
+    x = Conv2D(32, (5, 5), padding='same', strides=(2,2), name='d_conv1',
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(inputs)
+    #x = InstanceNormalization()(x)
+    #x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='d_conv1_bn')(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    
+    x = Conv2D(64, (5, 5), padding='same', strides=(2,2), name='d_conv2',
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(x)
+    #x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='d_conv2_bn')(x)
+    #x = InstanceNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    
+    x = Conv2D(128, (5, 5), padding='same', strides=(2,2), name='d_conv3',
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(x)
+    #x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='d_conv3_bn')(x)
+    #x = InstanceNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    
+    x = Conv2D(256, (5, 5), padding='same', strides=(2,2), name='d_conv4',
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(x)
+    #x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='d_conv4_bn')(x)
+    #x = InstanceNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    
+    x = Flatten()(x)
+    #x = Dense(2048, activation='relu', name='d_dense1',
+    #    kernel_initializer=RN(mean=0.0, stddev=0.02), bias_initializer=Constant())(x)
+    x = Dense(1, activation='sigmoid', name='d_out',
+        kernel_initializer=RN(mean=0.0, stddev=0.02), bias_initializer=Constant())(x)
+    model = Model(inputs=inputs, outputs=x, name='D')
     
     return model
 
 #Generatorモデル
 def G_model(Height, Width, channel=3):
-    in_h = int(Height / 4)
-    in_w = int(Width / 4)
+    inputs = Input((100,))
     
-    model = Sequential()
-    model.add(Dense(input_dim=100, output_dim=1024))
-    model.add(Activation('tanh'))
-    model.add(Dense(128 * in_h * in_w))
-    model.add(BatchNormalization())
-    model.add(Activation('tanh'))
-    model.add(Reshape((in_h, in_w, 128), input_shape=(128 * in_h * in_w,)))
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Conv2D(64, (5, 5), padding='same'))
-    model.add(Activation('tanh'))
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Conv2D(3, (5, 5), padding='same'))
-    model.add(Activation('tanh'))
+    in_h = int(Height / 16)
+    in_w = int(Width / 16)
     
-    return model    
+    d_dim = 512
+    
+    x = Dense(in_h * in_w * d_dim, name='g_dense1',
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(inputs)
+    x = Reshape((in_h, in_w, d_dim), input_shape=(d_dim * in_h * in_w,))(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='g_dense1_bn')(x)
+
+    # 1/8
+    #x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2DTranspose(512, (5, 5), name='g_conv1', padding='same', strides=(2,2),
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(x)
+    #x = Conv2D(256, (5, 5), padding='same', name='g_conv1',
+    #    kernel_initializer=RN(mean=0.0, stddev=0.02), bias_initializer=Constant())(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='g_conv1_bn')(x)
+
+    # 1/4
+    #x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2DTranspose(256, (5, 5), name='g_conv2', padding='same', strides=(2,2),
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(x)
+    #x = Conv2D(128, (5, 5), padding='same', name='g_conv2',
+    #    kernel_initializer=RN(mean=0.0, stddev=0.02), bias_initializer=Constant())(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='g_conv2_bn')(x)
+
+    # 1/2
+    #x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2DTranspose(128, (5, 5), name='g_conv3', padding='same', strides=(2,2),
+        kernel_initializer=RN(mean=0.0, stddev=0.02), use_bias=False)(x)
+    #x = Conv2D(64, (5, 5), padding='same', name='g_conv3',
+    #    kernel_initializer=RN(mean=0.0, stddev=0.02), bias_initializer=Constant())(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization(momentum=0.9, epsilon=1e-5, name='g_conv3_bn')(x)
+
+    # 1/1
+    #x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2DTranspose(channel, (5, 5), name='g_out', padding='same', strides=(2,2),
+        kernel_initializer=RN(mean=0.0, stddev=0.02),  bias_initializer=Constant())(x)
+    #x = Conv2D(channel, (5, 5), padding='same', activation='tanh', name='g_out',
+    #    kernel_initializer=RN(mean=0.0, stddev=0.02), bias_initializer=Constant())(x)
+    x = Activation('tanh')(x)
+    model = Model(inputs=inputs, outputs=x, name='G')
+
+    return model
 
 def generator_containing_discremenator(dis, gen):
     model = Sequential()
