b9e91faeb77db112c6dd906b4b23a14c3f510b36
yosuke1974 <yosuke.hayashi.11@gmail.com>
2019-04-30 17:36:14 +0900

    File replace

--- cifar10_dcgan_rev1.py
+++ cifar10_dcgan_rev1.py
@@ -126,13 +126,13 @@ def combine_images(generated_images):
     num = generated_images.shape[0]
     width = int(math.sqrt(num))
     height = int(math.ceil(float(num)/width))
-    shape = generated_images.shape[1:3]
-    image = np.zeros((height * shape[0], width * shape[1]), 
+    shape = generated_images.shape[1:4]
+    image = np.zeros((height * shape[0], width * shape[1], shape[2]), 
                      dtype=generated_images.dtype)
     for index, img in enumerate(generated_images):
         i = int(index/width)
         j = index % width
-        image[i * shape[0]:(i+1) * shape[0], j * shape[1]:(j+1) * shape[1]] = img[:, :, 0]
+        image[i * shape[0]:(i+1) * shape[0], j * shape[1]:(j+1) * shape[1], :] = img[:, :, :]
     
     return image
 
